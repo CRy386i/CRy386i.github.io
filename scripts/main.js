24adf2759cd726fc75a1144aba0c814aab850cf4
CRy386i <tetris123480@gmail.com>
2023-09-08 15:55:39 +0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var MyImage = document.querySelector("img");
+
+MyImage.onclick = function () {
+    var MySrc = MyImage.getAttribute("src");
+    if (MySrc === "images/1.webp") {
+	MyImage.setAttribute("src", "images/firefox2.webp");
+    } else {
+	MyImage.setAttribute("src", "images/1.webp");
+    }
+};
+
+var myButton = document.querySelector("button");
+var myHeading = document.querySelector("h1");
+
+function setUserName() {
+  var myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "Mozilla is cool, " + myName;
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla is cool, " + storedName;
+}
+myButton.onclick = function () {
+  setUserName();
+};
